5b2d2f22dfc5c475d405b12be34db6edbef99aa8
dousbao <dousbao.cjx@gmail.com>
2023-08-20 15:08:26 +0800

    add: move construct

--- include/cxxhazard/domain.hpp
+++ include/cxxhazard/domain.hpp
@@ -11,14 +11,30 @@ namespace cxxhazard {
 class domain {
 public:
 	explicit domain(unsigned int max = 1000) : 
-		_resource(), _reclaim(max) {}
+		_resource(new resource_pool), _reclaim(new reclaim_pool(max)) {}
 
-	virtual ~domain(void) noexcept = default;
+	domain(const domain &) = delete;
+
+	domain(domain &&rhs) :
+		_resource(rhs._resource), _reclaim(rhs._reclaim)
+	{
+		rhs._resource = nullptr;
+		rhs._reclaim = nullptr;
+	}
+
+	virtual ~domain(void) noexcept
+	{
+		delete _resource;
+		delete _reclaim;
+	}
+
+	domain &operator=(const domain &rhs) = delete;
+	domain &operator=(domain &&rhs) = delete;
 
 public:
 	inline hazard_ptr make_hazard(void)
 	{
-		return hazard_ptr(_resource.acquire());
+		return hazard_ptr(_resource->acquire());
 	}
 
 	template <typename T>
@@ -30,13 +46,13 @@ public:
 	template <typename T, typename Func>
 	void retire(T *ptr, Func &&deleter)
 	{
-		if (_reclaim.emplace_back(ptr, std::forward<Func>(deleter)) >= _reclaim._max) {
+		if (_reclaim->emplace_back(ptr, std::forward<Func>(deleter)) >= _reclaim->_max) {
 			std::unordered_set<void *> is_hazard;
 
-			for (auto p = _resource._head.load(); p != nullptr; p = p->_next)
+			for (auto p = _resource->_head.load(); p != nullptr; p = p->_next)
 				is_hazard.insert(p->_ptr.load());
 
-			_reclaim.reclaim([this, &is_hazard](void *ptr){
+			_reclaim->reclaim([this, &is_hazard](void *ptr){
 				if (is_hazard.count(ptr) != 0)
 					return true;
 				return false;
@@ -45,8 +61,8 @@ public:
 	}
 
 private:
-	resource_pool _resource;
-	reclaim_pool _reclaim;
+	resource_pool *_resource;
+	reclaim_pool *_reclaim;
 };
 
 } // namespace cxxhazard
